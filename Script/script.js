4cdd0f9b656d8e3c0f2d26d0f97520482d26fec2
Hellerson Murilo B Calabianqui de Souza - Libbs <hellerson.souza@libbs.com.br>
2023-10-04 23:08:50 -0300

    melhorias front e js

--- Script/script.js
+++ Script/script.js
@@ -1,14 +1,22 @@
 //importando a variavel global chave API
-import {env} from "../env.js";
 
-var chaveApi = env.chaveApi
-var url = `https://superheroapi.com/api/${chaveApi}/`
+
+var chaveApi = 1032032714593650
+var urlId = `https://superheroapi.com/api/${chaveApi}/` //valor URL pelo o ID
+var urlNome = `https://superheroapi.com/api/${chaveApi}/search/` //valor URL pelo o nome do heroi
 
 var btnBuscar = document.getElementById('btnBuscar')
 
 //obter o ID pelo ao clicar no botao
 btnBuscar.addEventListener('click', () =>{
-    var campoBusca = document.getElementById('campoBusca')
-    url = `https://superheroapi.com/api/${chaveApi}/${campoBusca.value}`
-    console.log(url)
+    let campoBusca = document.getElementById('campoBusca')
+    urlId +=`${campoBusca.value}`
+    console.log(urlId)
+})
+
+//obter o nome do heroi pelo ao clicar no botao
+btnBuscar.addEventListener('click', () =>{
+    let campoBusca = document.getElementById('campoBusca')
+    urlNome += `${campoBusca.value}`
+    console.log(urlNome)
 })
