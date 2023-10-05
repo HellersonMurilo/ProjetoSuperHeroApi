c9542f16fa490a6ad90266913820f61e97f75b2e
Hellerson Murilo B Calabianqui de Souza - Libbs <hellerson.souza@libbs.com.br>
2023-10-05 08:30:29 -0300

    melhoria validação de erros

--- Script/script.js
+++ Script/script.js
@@ -8,15 +8,33 @@ var urlNome = `https://superheroapi.com/api/${chaveApi}/search/` //valor URL pel
 var btnBuscar = document.getElementById('btnBuscar')
 
 //obter o ID pelo ao clicar no botao
-btnBuscar.addEventListener('click', () =>{
+btnBuscar.addEventListener('click', () => {
     let campoBusca = document.getElementById('campoBusca')
-    urlId +=`${campoBusca.value}`
+    urlId += `${campoBusca.value}`
     console.log(urlId)
+
+
 })
 
 //obter o nome do heroi pelo ao clicar no botao
-btnBuscar.addEventListener('click', () =>{
-    let campoBusca = document.getElementById('campoBusca')
-    urlNome += `${campoBusca.value}`
-    console.log(urlNome)
-})
+btnBuscar.addEventListener('click', () => {
+    let campoBusca = document.getElementById('campoBusca').value.trim(); // Remova espaços em branco do início e do fim
+    if (campoBusca !== "") {
+        urlNome += `${campoBusca}`//console.log(urlNome)
+        try {
+            fetch(urlNome, { method: "get" })
+                .then(resultado => resultado.json())
+                .then(dados => showInfo(dados))
+            function showInfo(dados) {
+                console.log(dados);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }else{
+        alert("Informe o nome ou ID");
+    }
+}
+)
+
+
