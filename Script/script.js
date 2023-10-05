4e302b73c9e38d9c365e36787615a30dce63b92f
Hellerson Murilo B Calabianqui de Souza - Libbs <hellerson.souza@libbs.com.br>
2023-10-05 08:40:02 -0300

    melhoria validação de erros

--- Script/script.js
+++ Script/script.js
@@ -1,40 +1,36 @@
-//importando a variavel global chave API
+// Importando a variável global chave API
+var chaveApi = 1032032714593650;
+var urlBase = "https://superheroapi.com/api/" + chaveApi + "/";
 
+var btnBuscar = document.getElementById('btnBuscar');
 
-var chaveApi = 1032032714593650
-var urlId = `https://superheroapi.com/api/${chaveApi}/` //valor URL pelo o ID
-var urlNome = `https://superheroapi.com/api/${chaveApi}/search/` //valor URL pelo o nome do heroi
-
-var btnBuscar = document.getElementById('btnBuscar')
-
-//obter o ID pelo ao clicar no botao
 btnBuscar.addEventListener('click', () => {
-    let campoBusca = document.getElementById('campoBusca')
-    urlId += `${campoBusca.value}`
-    console.log(urlId)
+    let campoBusca = document.getElementById('campoBusca').value.trim();
 
-
-})
-
-//obter o nome do heroi pelo ao clicar no botao
-btnBuscar.addEventListener('click', () => {
-    let campoBusca = document.getElementById('campoBusca').value.trim(); // Remova espaços em branco do início e do fim
-    if (campoBusca !== "") {
-        urlNome += `${campoBusca}`//console.log(urlNome)
+    //Validar se o input foi o ID ou o nome
+    if (campoBusca !== "") {// Verificar se o campo de busca é um número (ID) ou uma string (nome)
+        let url;
+        if (!isNaN(campoBusca)) {// Se for um número, é ID
+            url = urlBase + campoBusca;
+            console.log("era numero");
+        } else {// Se for uma string, é nome do herói
+            console.log("sou texto carai")
+            url = urlBase + "search/" + campoBusca;
+        }
         try {
-            fetch(urlNome, { method: "get" })
+            fetch(url, { method: "get" })
                 .then(resultado => resultado.json())
-                .then(dados => showInfo(dados))
+                .then(dados => showInfo(dados));
+
             function showInfo(dados) {
                 console.log(dados);
             }
         } catch (error) {
             console.log(error);
         }
-    }else{
+    } else {
         alert("Informe o nome ou ID");
     }
-}
-)
+});
 
 
