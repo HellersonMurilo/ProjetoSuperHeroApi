73c4a780cc5af677bd00327a1cac0194b6c19372
Hellerson Murilo B Calabianqui de Souza - Libbs <hellerson.souza@libbs.com.br>
2023-10-10 11:17:14 -0300

    melhoria do código

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ const btnBuscar = document.getElementById('btnBuscar')
 
 btnBuscar.addEventListener('click', () => {
     var campoBusca = document.getElementById("campoBusca").value
-    let url;
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTZjY2YxMDUwYzIyYTc2MTI4MmVhMDM2ZjllYjZkOCIsInN1YiI6IjY1MjIwOGYyYzUwYWQyMDBhZDg0ZjAzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EszvEiolp95keL1Y62nHD2i9Ih41mHNDb49HGwedOdc'
-        }
-    };
+    if (campoBusca != "") {
+        let url;
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTZjY2YxMDUwYzIyYTc2MTI4MmVhMDM2ZjllYjZkOCIsInN1YiI6IjY1MjIwOGYyYzUwYWQyMDBhZDg0ZjAzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EszvEiolp95keL1Y62nHD2i9Ih41mHNDb49HGwedOdc'
+            }
+        };
 
-    url = 'https://api.themoviedb.org/3/search/movie?query=' + campoBusca + '&include_adult=false&language=pt-BR&page=1'
-    console.log(url)
+        url = 'https://api.themoviedb.org/3/search/movie?query=' + campoBusca + '&include_adult=false&language=pt-BR&page=1'
+        console.log(url)
 
-    fetch(url, options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+        fetch(url, options)
+            .then(response => response.json())
+            .then(response => (showInfo(response)))
+            .catch(err => console.error(err));
+    }else{
+        alert('Nome filme não informado')
+    }
+
+    function showInfo(response) {
+        console.log(response.results[0].original_title)
+    }
+
 })
