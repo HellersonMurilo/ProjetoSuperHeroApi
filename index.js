c08cc83e49d5ae42b952909403e1e674de95b001
HellersonMurilo <calabianquimurilo328@gmail.com>
2023-10-18 21:13:08 -0300

    melhorias

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const options = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer' + accessToken
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTZjY2YxMDUwYzIyYTc2MTI4MmVhMDM2ZjllYjZkOCIsInN1YiI6IjY1MjIwOGYyYzUwYWQyMDBhZDg0ZjAzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EszvEiolp95keL1Y62nHD2i9Ih41mHNDb49HGwedOdc'
     }
 };
 
@@ -22,5 +22,7 @@ fetch(urlTendencias, options)
 
 //função que realizar o printar os resultados
 function showInfo(response) {
-    console.log(response)
+    response.results.forEach(element => {
+        console.log(element.title)
+    });
 }
