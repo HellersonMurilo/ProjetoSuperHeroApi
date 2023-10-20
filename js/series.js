b1dd59152bca6d19325300d1b32a38e6e85bc723
Your Name <you@example.com>
2023-10-20 00:24:12 -0300

    Desenvolvendo tela de séries

---
+++ js/series.js
@@ -0,0 +1,37 @@
+//chaves de acesso
+const apiKey = "596ccf1050c22a761282ea036f9eb6d8"
+const accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTZjY2YxMDUwYzIyYTc2MTI4MmVhMDM2ZjllYjZkOCIsInN1YiI6IjY1MjIwOGYyYzUwYWQyMDBhZDg0ZjAzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EszvEiolp95keL1Y62nHD2i9Ih41mHNDb49HGwedOdc"
+
+//url das series populares
+var urlPopularSeries = "https://api.themoviedb.org/3/tv/top_rated?language=pt-BR"
+
+//metodos para parametros no fetch
+var options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTZjY2YxMDUwYzIyYTc2MTI4MmVhMDM2ZjllYjZkOCIsInN1YiI6IjY1MjIwOGYyYzUwYWQyMDBhZDg0ZjAzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EszvEiolp95keL1Y62nHD2i9Ih41mHNDb49HGwedOdc'
+    }
+};
+
+fetch(urlPopularSeries, options)
+    .then(response => response.json())
+    .then(response => showData(response))
+
+const divPai = document.getElementById("paiPopular")
+
+function showData(response) {
+
+    console.log(response)
+    response.results.forEach(element => {
+        let postSerie = document.createElement('div')
+        postSerie.innerHTML = `
+        <div class="postSerie">
+            <img src="https://image.tmdb.org/t/p/w200${element.poster_path}" alt="">
+            <p>${element.name}</p>
+            <p>⭐${element.vote_average}</p>
+        </div>
+        `
+        divPai.appendChild(postSerie)
+    });
+}
